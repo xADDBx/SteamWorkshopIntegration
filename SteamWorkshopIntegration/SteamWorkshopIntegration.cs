22d1c3e81b3c2dfc1ffe116bf7e1ab6b700d6119
ADDB <addb1234567890@gmail.com>
2023-12-20 06:23:12 +0100

    Finished Implementation

--- SteamWorkshopIntegration/SteamWorkshopIntegration.cs
+++ SteamWorkshopIntegration/SteamWorkshopIntegration.cs
@@ -1,5 +1,7 @@
-﻿using Kingmaker.Utility.NewtonsoftJson;
+﻿using Kingmaker.EntitySystem.Interfaces;
+using Kingmaker.Utility.NewtonsoftJson;
 using Kingmaker.Utility.UnityExtensions;
+using Newtonsoft.Json;
 using Steamworks;
 using System;
 using System.Collections.Generic;
@@ -8,18 +10,19 @@ using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using static UnityEngine.PlayerLoop.PreUpdate;
 
 namespace Kingmaker.Modding {
     public class SteamWorkshopIntegration {
         public static bool Started { get; private set; }
-        // This file contains information about the latest installed version
-        public const string ModManagingInfoFileName = "WorkshopManaged.json";
         private static string SettingsFilePath => Path.Combine(ApplicationPaths.persistentDataPath, OwlcatModificationsManager.SettingsFileName);
-        private static string DefaultOwlcatTemplateDirectory => Path.Combine(ApplicationPaths.persistentDataPath, "Modifications");
-        private static string DefaultUMMDirectory => Path.Combine(ApplicationPaths.persistentDataPath, "UnityModManager");
+        internal static string DefaultOwlcatTemplateDirectory => Path.Combine(ApplicationPaths.persistentDataPath, "Modifications");
+        internal static string DefaultUMMDirectory => Path.Combine(ApplicationPaths.persistentDataPath, "UnityModManager");
         private static string UnityModManagerFilesZipPath => Path.Combine(ApplicationPaths.streamingAssetsPath, "OwlcatUnityModManager.zip");
         private static string[] UnityModManagerFileNames = new[] { "dnlib.dll", "Ionic.Zip.dll", "UnityModManager.dll", "UnityModManager.pdb" };
-        private OwlcatModificationsManager.SettingsData m_Settings; 
+        internal WriteableSettingsData m_Settings;
+        public Dictionary<PublishedFileId_t, SteamWorkshopModification> SubscribedModifications = new();
 
         private static SteamWorkshopIntegration s_Instance;
         public static SteamWorkshopIntegration Instance {
@@ -32,6 +35,7 @@ namespace Kingmaker.Modding {
         }
         private SteamWorkshopIntegration() {
             if (!SteamAPI.Init()) {
+                // After everything works this could possibly removed because when this returns false the issue is pretty much unrelated (game not launched via Steam; Steam installation problematic etc.)
                 PFLog.Mods.Log("Could not initialize SteamAPI.");
                 return;
             }
@@ -45,18 +49,91 @@ namespace Kingmaker.Modding {
                 EnsureUnityModManagerFiles();
                 EnsureTemplateDirectoryAndSettingsFile();
             } catch (Exception ex) {
-                PFLog.Mods.Exception(ex, null);
+                PFLog.Mods.Exception(ex);
                 return;
             }
-            Start();
         }
         public void Start() {
             if (Started) {
                 return;
             }
             Started = true;
+            try {
+                GetSubscribedModifications();
+                GetInstalledModifications();
+                InstallOrUpdateSubscribedModifications();
+                File.WriteAllText(SettingsFilePath, JsonUtility.ToJson(m_Settings, true));
+            } catch (Exception ex) {
+                PFLog.Mods.Exception(ex);
+                return;
+            }
         }
-
+        private void InstallOrUpdateSubscribedModifications() {
+            foreach (var mod in SubscribedModifications.Values) {
+                if (!mod.IsDownloading) {
+                    if (mod.WorkshopSettings == null) {
+                        mod.Install();
+                    } else if (mod.LocalTimestamp < mod.SteamTimestamp) {
+                        mod.Install(true);
+                    }
+                }
+            }
+        }
+        private void GetInstalledModifications() {
+            foreach (var directory in Directory.GetDirectories(DefaultUMMDirectory)) {
+                var workshopManagingFile = new FileInfo(Path.Combine(directory, SteamWorkshopModification.WorkshopSettingsData.ModManagingInfoFileName));
+                if (workshopManagingFile.Exists) {
+                    try {
+                        var workshopSettings = JsonUtility.FromJson<SteamWorkshopModification.WorkshopSettingsData>(File.ReadAllText(workshopManagingFile.FullName));
+                        if (workshopSettings == null) throw new Exception();
+                        if (SubscribedModifications.TryGetValue(workshopSettings.SteamFileId, out var mod)) {
+                            mod.WorkshopSettings = workshopSettings;
+                        } else {
+                            UninstallModification(directory, true, workshopSettings.UniqueName);
+                        }
+                    } catch (Exception) {
+                        UninstallModification(directory, true);
+                    }
+                }
+            }
+            foreach (var directory in Directory.GetDirectories(DefaultOwlcatTemplateDirectory)) {
+                var workshopManagingFile = new FileInfo(Path.Combine(directory, SteamWorkshopModification.WorkshopSettingsData.ModManagingInfoFileName));
+                if (workshopManagingFile.Exists) {
+                    try {
+                        var workshopSettings = JsonUtility.FromJson<SteamWorkshopModification.WorkshopSettingsData>(File.ReadAllText(workshopManagingFile.FullName));
+                        if (workshopSettings == null) throw new Exception();
+                        if (SubscribedModifications.TryGetValue(workshopSettings.SteamFileId, out var mod)) {
+                            mod.WorkshopSettings = workshopSettings;
+                        } else {
+                            UninstallModification(directory, false, workshopSettings.UniqueName);
+                        }
+                    } catch (Exception) {
+                        UninstallModification(directory, false);
+                    }
+                }
+            }
+        }
+        private void UninstallModification(string ModificationDirectory, bool isUmm, string UniqueName = null) {
+            PFLog.Mods.Log($"Deleting unsubscribed modification: {UniqueName ?? "Unknown Modification"} at {ModificationDirectory}");
+            try {
+                Directory.Delete(ModificationDirectory, true);
+                if (!isUmm && !UniqueName.IsNullOrEmpty()) {
+                    var newList = m_Settings.EnabledModifications.ToList();
+                    newList.Remove(UniqueName);
+                    m_Settings.EnabledModifications = newList.ToArray();
+                }
+            } catch (Exception ex) {
+                PFLog.Mods.Exception(ex);
+            }
+        }
+        private void GetSubscribedModifications() {
+            uint numSubscribedMods = SteamUGC.GetNumSubscribedItems();
+            PublishedFileId_t[] subscribedItems = new PublishedFileId_t[numSubscribedMods];
+            SteamUGC.GetSubscribedItems(subscribedItems, numSubscribedMods);
+            foreach (var mod in subscribedItems) {
+                SubscribedModifications[mod] = new SteamWorkshopModification(mod);
+            }
+        }
         private void EnsureUnityModManagerFiles() {
             var ummDirectory = new DirectoryInfo(DefaultUMMDirectory);
             if (ummDirectory.Exists) {
@@ -70,25 +147,34 @@ namespace Kingmaker.Modding {
             }
         }
         private void ReinstallUnityModManager() {
-            var unzipDir = new DirectoryInfo(Path.Combine(Path.GetTempPath(), "UMMAssets"));
-            unzipDir.Create();
-            ZipFile.ExtractToDirectory(UnityModManagerFilesZipPath, unzipDir.FullName);
-            foreach (var file in UnityModManagerFileNames) {
-                var target = new FileInfo(Path.Combine(DefaultUMMDirectory, file));
-                if (target.Exists) {
-                    target.Delete();
+            PFLog.Mods.Log($"Copying UnityModManager Files from {UnityModManagerFilesZipPath} to {DefaultUMMDirectory}.");
+            var unzipDir = new DirectoryInfo(Path.Combine(Path.GetTempPath(), "RogueTraderUMMAssets"));
+            try {
+                if (unzipDir.Exists) {
+                    unzipDir.Delete(true);
                 }
-                var source = new FileInfo(Path.Combine(unzipDir.FullName, file));
-                source.MoveTo(target.FullName);
+                unzipDir.Create();
+                ZipFile.ExtractToDirectory(UnityModManagerFilesZipPath, unzipDir.FullName);
+                foreach (var file in UnityModManagerFileNames) {
+                    var target = new FileInfo(Path.Combine(DefaultUMMDirectory, file));
+                    if (target.Exists) {
+                        target.Delete();
+                    }
+                    var source = new FileInfo(Path.Combine(unzipDir.FullName, "UnityModManager", file));
+                    source.MoveTo(target.FullName);
+                }
+            } finally {
+                if (unzipDir.Exists) {
+                    unzipDir.Delete(true);
+                }
             }
-            unzipDir.Delete(true);
         }
         private void EnsureTemplateDirectoryAndSettingsFile() {
             var owlcatTemplateDirectory = new DirectoryInfo(DefaultOwlcatTemplateDirectory);
             if (!owlcatTemplateDirectory.Exists) owlcatTemplateDirectory.Create();
             if (File.Exists(SettingsFilePath)) {
                 try {
-                    m_Settings = NewtonsoftJsonHelper.DeserializeFromFile<OwlcatModificationsManager.SettingsData>(SettingsFilePath);
+                    m_Settings = JsonUtility.FromJson<WriteableSettingsData>(File.ReadAllText(SettingsFilePath));
                     if (m_Settings == null) throw new Exception();
                 } catch (Exception) {
                     File.Delete(SettingsFilePath);
@@ -99,8 +185,16 @@ namespace Kingmaker.Modding {
             }
         }
         private void RecreateTemplateModSettingsFile() {
+            PFLog.Mods.Log($"Creating default OwlcatModificationManagerSettings.json in {SettingsFilePath}");
             m_Settings = new();
-            NewtonsoftJsonHelper.SerializeToFile(SettingsFilePath, m_Settings, true);
+            File.WriteAllText(SettingsFilePath, JsonUtility.ToJson(m_Settings, true));
+        }
+        public class WriteableSettingsData {
+            [JsonProperty]
+            public string[] SourceDirectories = new string[0];
+
+            [JsonProperty]
+            public string[] EnabledModifications = new string[0];
         }
     }
 }
---
+++ SteamWorkshopIntegration/SteamWorkshopModification.cs
@@ -0,0 +1,171 @@
+﻿using Kingmaker.Utility.NewtonsoftJson;
+using Kingmaker.Utility.UnityExtensions;
+using Newtonsoft.Json;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static Kingmaker.Modding.SteamWorkshopIntegration;
+
+namespace Kingmaker.Modding {
+    public class SteamWorkshopModification {
+        public const string UMMInfoFileName = "Info.json";
+        public PublishedFileId_t SteamFileId;
+        public WorkshopSettingsData WorkshopSettings;
+        public string SteamLocation;
+        public string LocalLocation;
+        public OwlcatModificationManifest Manifest = null;
+        public bool IsUmm = false;
+        public bool IsOwlcatTemplate = false;
+        public bool IsDownloading = false;
+        public bool NeedsDownload;
+        public uint SteamTimestamp;
+        public uint LocalTimestamp => WorkshopSettings?.LocalTimestamp ?? 0;
+
+        public SteamWorkshopModification(PublishedFileId_t mod) {
+            SteamFileId = mod;
+            var state = (EItemState)SteamUGC.GetItemState(mod);
+            var downloaded = state.HasFlag(EItemState.k_EItemStateInstalled);
+            var hasUpdate = state.HasFlag(EItemState.k_EItemStateNeedsUpdate);
+            IsDownloading = state.HasFlag(EItemState.k_EItemStateDownloading);
+            NeedsDownload = (hasUpdate || !downloaded) && !IsDownloading;
+            if (downloaded) {
+                SteamUGC.GetItemInstallInfo(mod, out var size, out var dir, 256, out var timestamp);
+                SteamLocation = dir;
+                SteamTimestamp = timestamp;
+            }
+        }
+        public void Install(bool isUpdate = false) {
+            PFLog.Mods.Log($"{(isUpdate ? "Updating" : "Installing")} Steam Workshop with Id: " + SteamFileId);
+            var unzipDir = new DirectoryInfo(Path.Combine(Path.GetTempPath(), "RogueTraderWorkshopMod" + SteamFileId.m_PublishedFileId.ToString()));
+            try {
+                if (unzipDir.Exists) {
+                    unzipDir.Delete(true);
+                }
+                unzipDir.Create();
+                // Files are either available as
+                // 1. _legacy.bin archive; In which case SteamLocation points to a file
+                // 2. .zip; In which case SteamLocation points to the directory containing the zip
+                // 3. unzipped files; In which case SteamLocation points to the directory containing the files
+                string pathToZip = null;
+                if (SteamLocation.EndsWith(@"_legacy.bin")) {
+                    pathToZip = SteamLocation;
+                } else {
+                    var zips = Directory.GetFiles(SteamLocation, "*.zip");
+                    if (zips.Length > 0) {
+                        // I'll assume there will only ever be one zip?
+                        pathToZip = Path.Combine(SteamLocation, zips.First());
+                    }
+                }
+                if (pathToZip != null) {
+                    ZipFile.ExtractToDirectory(pathToZip, unzipDir.FullName);
+                } else {
+                    CopyDirectoryRecursively(new DirectoryInfo(SteamLocation), unzipDir.FullName);
+                }
+                // Workaround for nested zips; Why do they even exist
+                // Some mod might, for whatever reason, want to ship zip files. To prevent accidentally unpacking them too this will only continue until there are other files besides the single zip
+                int i = 0;
+                while (unzipDir.GetFiles().Length == 1 && unzipDir.GetFiles("*.zip")?.Length == 1) {
+                    i += 1;
+                    if (i > 10) {
+                        throw new Exception("Error while trying to Install Workshop Item. Exceeded 10 nested zip archives.");
+                    }
+                    var newUnzipDir = new DirectoryInfo(Path.Combine(Path.GetTempPath(), "RogueTraderWorkshopMod" + SteamFileId.m_PublishedFileId.ToString() + "_nested"));
+                    if (newUnzipDir.Exists) {
+                        newUnzipDir.Delete(true);
+                    }
+                    try {
+                        newUnzipDir.Create();
+                        var archive = unzipDir.GetFiles().First();
+                        ZipFile.ExtractToDirectory(archive.FullName, newUnzipDir.FullName);
+                        archive.Delete();
+                        CopyDirectoryRecursively(newUnzipDir, unzipDir.FullName);
+                    } catch (Exception ex) {
+                        throw ex;
+                    } finally {
+                        if (newUnzipDir?.Exists ?? false) {
+                            newUnzipDir.Delete(true);
+                        }
+                    }
+                }
+                var manifestPath = Path.Combine(unzipDir.FullName, OwlcatModification.ManifestFileName);
+                if (File.Exists(manifestPath)) {
+                    try {
+                        Manifest = JsonUtility.FromJson<OwlcatModificationManifest>(File.ReadAllText(manifestPath));
+                    } catch (Exception ex) {
+                        PFLog.Mods.Exception(ex);
+                    }
+                }
+                string targetDir = null;
+                if (Manifest != null) {
+                    PFLog.Mods.Log($"Steam Workshop Item recognized as {Manifest.DisplayName} with Id {Manifest.UniqueName}");
+                    if (!Manifest.UniqueName.IsNullOrEmpty()) {
+                        if (unzipDir.GetFiles()?.Where(file => file.Name == UMMInfoFileName)?.Any() ?? false) {
+                            IsUmm = true;
+                            targetDir = Path.Combine(DefaultUMMDirectory, Manifest.UniqueName);
+                        } else if (unzipDir.GetFiles()?.Where(file => file.Name == OwlcatModification.SettingsFileName)?.Any() ?? false) {
+                            IsOwlcatTemplate = true;
+                            targetDir = Path.Combine(DefaultOwlcatTemplateDirectory, Manifest.UniqueName);
+                            var newList = SteamWorkshopIntegration.Instance.m_Settings.EnabledModifications.ToList();
+                            if (!newList.Contains(Manifest.UniqueName)) {
+                                newList.Add(Manifest.UniqueName);
+                                SteamWorkshopIntegration.Instance.m_Settings.EnabledModifications = newList.ToArray();
+                            }
+                        } else {
+                            // This is a case that won't be handled. I've heard of people trying to upload other workshop content like custom portraits; which would end up here
+                        }
+                    } else {
+                        PFLog.Mods.Error($"Steam Workshop Item {Manifest.DisplayName} with Steam FileId {SteamFileId} has null or empty UniqueName and can't be installed");
+                    }
+                } else {
+                    // Theoretically UMM mods should be able to run without a manifest, but since shipping without it is actually pretty hard to realize this will be ignored
+                }
+                if ((IsUmm || IsOwlcatTemplate) && !targetDir.IsNullOrEmpty()) {
+                    CopyDirectoryRecursively(unzipDir, targetDir);
+                    if (!isUpdate) WorkshopSettings = new();
+                    WorkshopSettings.LocalTimestamp = SteamTimestamp;
+                    WorkshopSettings.UniqueName = Manifest.UniqueName;
+                    WorkshopSettings.SteamFileId = SteamFileId;
+                    File.WriteAllText(Path.Combine(targetDir, WorkshopSettingsData.ModManagingInfoFileName), JsonUtility.ToJson(WorkshopSettings, true));
+                }
+            } catch (Exception ex) {
+                PFLog.Mods.Exception(ex);
+            } finally {
+                if (unzipDir?.Exists ?? false) {
+                    unzipDir.Delete(true);
+                }
+            }
+        }
+
+        public static void CopyDirectoryRecursively(DirectoryInfo dir, string target) {
+            var targetDir = new DirectoryInfo(target);
+            if (!targetDir.Exists) {
+                targetDir.Create();
+            }
+            foreach (var file in dir.GetFiles()) {
+                string path = Path.Combine(target, file.Name);
+                if (File.Exists(path)) File.Delete(path);
+                file.CopyTo(path);
+            }
+            foreach (var directory in dir.GetDirectories()) {
+                CopyDirectoryRecursively(directory, Path.Combine(target, directory.Name));
+            }
+        }
+        public class WorkshopSettingsData {
+            // This file contains information about the latest installed version
+            [JsonProperty]
+            public const string ModManagingInfoFileName = "WorkshopManaged.json";
+            [JsonProperty]
+            public uint LocalTimestamp;
+            [JsonProperty]
+            public PublishedFileId_t SteamFileId;
+            [JsonProperty]
+            public string UniqueName;
+        }
+    }
+}
